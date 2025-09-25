b02907367c0fed90486182f44818b36dcd3ff709
Steven Alvin Christian <5025241116@student.its.ac.id>
2025-09-26 00:29:00 +0700

    Update profile.js

--- Quiz1/profile.js
+++ Quiz1/profile.js
@@ -1,44 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const gallery = document.querySelector(".journey-gallery");
-  
-    if (gallery) {
-      const cards = Array.from(gallery.children);
-      const len = card.length;
-  
-      cards.forEach(card => {
-        const dupe = card.cloneNode(true);
-        dupe.setAttribute('aria-hidden', true); 
-        gallery.appendChild(dupe);
-      });
-  
-      let scroll = 0;
-      const scrollSpeed = 0.5;
-      let pause = false;
-      let frameid;
-  
-      function anims_scroll() {
-        if (pause) {
-          frameid = requestAnimationFrame(anims_scroll);
-          return;
-        }
-        
-        scroll -= scrollSpeed;
-        
-        const offset = (card[0].offsetWidth + 30) * len;
-  
-        if (Math.abs(scroll) >= offset) {
-          scroll = 0;
-        }
-  
-        gallery.style.transform = `translateX(${scroll}px)`;
-        
-        frameid = requestAnimationFrame(anims_scroll);
+  const gallery = document.querySelector(".journey-gallery");
+
+  if (gallery) {
+    const cards = Array.from(gallery.children);
+    const len = cards.length;
+
+    cards.forEach(c => {
+      const dupe = c.cloneNode(true);
+      dupe.setAttribute("aria-hidden", true);
+      gallery.appendChild(dupe);
+    });
+
+    let scroll = 0;
+    const scrollSpeed = 0.5;
+    let pause = false;
+    let frameId;
+
+    function animsScroll() {
+      if (pause) {
+        frameId = requestAnimationFrame(animsScroll);
+        return;
       }
-  
-      gallery.addEventListener('mouseenter', () => pause = true);
-      gallery.addEventListener('mouseleave', () => pause = false);
-      
-      anims_scroll();
+
+      scroll -= scrollSpeed;
+
+      const offset = (cards[0].offsetWidth + 30) * len;
+
+      if (Math.abs(scroll) >= offset) {
+        scroll = 0;
+      }
+
+      gallery.style.transform = `translateX(${scroll}px)`;
+
+      frameId = requestAnimationFrame(animsScroll);
     }
-  });
-  
+
+    gallery.addEventListener("mouseenter", () => (pause = true));
+    gallery.addEventListener("mouseleave", () => (pause = false));
+
+    animsScroll();
+  }
+});
